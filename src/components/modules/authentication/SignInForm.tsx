e4427c0664999f13a139dbaa36b3a9f4e106d6a0
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-02-05 20:57:35 +0600

    sign in init

--- src/components/modules/authentication/SignInForm.tsx
+++ src/components/modules/authentication/SignInForm.tsx
@@ -1,9 +1,26 @@
+'use client';
+
 import AuthCard from "./AuthCard";
 import { Button } from "@/components/ui/button";
 import FormInput from "@/components/ui/FormInput";
+import { useForm } from "@tanstack/react-form";
+import { signInSchema, SignInValues } from "./validation/auth.schema";
 import { signInFields } from "./config/authFields";
 
 export default function SignInForm() {
+    const form = useForm({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        validators: {
+            onChange: signInSchema,
+        },
+        onSubmit: async ({ value }) => {
+            console.log("SIGN IN", value);
+        },
+    });
+
     return (
         <div>
             <AuthCard
@@ -13,13 +30,34 @@ export default function SignInForm() {
                 linkText="Don't have an account?"
                 linkHref="/signup"
             >
-                {signInFields.map((field) => (
-                    <FormInput key={field.name} {...field} />
-                ))}
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        form.handleSubmit();
+                    }}
+                    className="space-y-5"
+                >
+                    {signInFields.map((f) => (
+                        <form.Field key={f.name} name={f.name as keyof SignInValues}>
+                            {(field) => <FormInput {...f} field={field} />}
+                        </form.Field>
+                    ))}
 
-                <Button className="w-full h-11 bg-emerald-600 hover:bg-emerald-700 shadow-md shadow-emerald-600/30">
-                    Login
-                </Button>
+
+                    <form.Subscribe
+                        selector={(state) => [state.canSubmit, state.isSubmitting]}
+                    >
+                        {([canSubmit, isSubmitting]) => (
+                            <Button
+                                type="submit"
+                                disabled={!canSubmit}
+                                className="w-full h-11 bg-emerald-600 hover:bg-emerald-700"
+                            >
+                                {isSubmitting ? "Creating..." : "Sign Up"}
+                            </Button>
+                        )}
+                    </form.Subscribe>
+                </form>
             </AuthCard>
         </div>
     )
--- src/components/modules/authentication/validation/auth.schema.ts
+++ src/components/modules/authentication/validation/auth.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const signInSchema = z.object({
     email: z.string().email("Enter a valid email"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
+    password: z.string().min(6, "Password must be at least 8 characters"),
 });
 
 export const signUpSchema = z
