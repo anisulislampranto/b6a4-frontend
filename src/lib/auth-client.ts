67c39ef2c60656e5669e2179934489bbd0481d33
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-04-20 22:34:12 +0600

    auth using base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 import { inferAdditionalFields } from "better-auth/client/plugins";
+import { API_BASE_URL } from "@/services/api-base";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5000",
+    baseURL: API_BASE_URL,
     plugins: [
         inferAdditionalFields({
             user: {
