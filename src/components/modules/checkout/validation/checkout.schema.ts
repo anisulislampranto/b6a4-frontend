ebfa55b37003cfdcb2d432ca4131a6eb9c78900a
anisulislampranto <76773444+anisulislampranto@users.noreply.github.com>
2026-04-21 22:54:12 +0600

    payment gatteway ssl init

--- src/components/modules/checkout/validation/checkout.schema.ts
+++ src/components/modules/checkout/validation/checkout.schema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const checkoutSchema = z.object({
     address: z.string().min(10, "Address must be at least 10 characters long"),
+    paymentMethod: z.enum(["COD", "SSL"]),
 });
 
 export type CheckoutValues = z.infer<typeof checkoutSchema>;
